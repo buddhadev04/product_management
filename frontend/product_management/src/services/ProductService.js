c7700cacb82a6d938dd5e10144df2b1d6ebf841b
insabhi-buddhadev <buddhadev@insabhi.com>
2025-02-23 16:33:34 +0530

    modify the baseurl

--- frontend/product_management/src/services/ProductService.js
+++ frontend/product_management/src/services/ProductService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8000";
+const baseURL = process.env.REACT_APP_BASE_URL || "https://product-management-lxrm.onrender.com"; 
 
 
 export const signUp = async (userData) => {
